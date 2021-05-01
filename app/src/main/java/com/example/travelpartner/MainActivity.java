7c02e64c14bcaa674eeff217795fdbf62324a251
HarshanaBuddhika <harshanabuddhika9@gmail.com>
2021-05-01 17:04:47 +0530

    Basic

---
+++ app/src/main/java/com/example/travelpartner/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.travelpartner;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView btmnavi=findViewById(R.id.bottom_navigation);
+
+        btmnavi.setSelectedItemId(R.id.home);
+
+        btmnavi.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+
+                    case R.id.home:
+                        return  true;
+
+                    case R.id.account:
+                        startActivity(new Intent(getApplicationContext(),Account.class));
+                        overridePendingTransition(0,0);
+                        return  true;
+
+                    case R.id.payment:
+                        startActivity(new Intent(getApplicationContext(),Payment.class));
+                        overridePendingTransition(0,0);
+                        return  true;
+
+                    case R.id.rent:
+                        startActivity(new Intent(getApplicationContext(),Rent.class));
+                        overridePendingTransition(0,0);
+                        return  true;
+
+                    case R.id.share:
+                        startActivity(new Intent(getApplicationContext(),Share.class));
+                        overridePendingTransition(0,0);
+                        return  true;
+                }
+
+                return false;
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/travelpartner/Share.java
@@ -0,0 +1,57 @@
+package com.example.travelpartner;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Share extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share);
+
+        BottomNavigationView btmnavi=findViewById(R.id.bottom_navigation);
+
+        btmnavi.setSelectedItemId(R.id.share);
+
+        btmnavi.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        overridePendingTransition(0,0);
+                        return  true;
+
+                    case R.id.account:
+                        startActivity(new Intent(getApplicationContext(),Account.class));
+                        overridePendingTransition(0,0);
+                        return  true;
+
+                    case R.id.payment:
+                        startActivity(new Intent(getApplicationContext(),Payment.class));
+                        overridePendingTransition(0,0);
+                        return  true;
+
+                    case R.id.rent:
+                        startActivity(new Intent(getApplicationContext(),Rent.class));
+                        overridePendingTransition(0,0);
+                        return  true;
+
+                    case R.id.share:
+                        return  true;
+                }
+
+                return false;
+            }
+        });
+    }
+}
