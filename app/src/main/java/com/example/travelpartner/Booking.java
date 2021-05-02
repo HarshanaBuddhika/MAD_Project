6be87f5c622eda9b975b70ac317284bd82733bac
HarshanaBuddhika <harshanabuddhika9@gmail.com>
2021-05-02 23:57:42 +0530

    Basic

---
+++ app/src/main/java/com/example/travelpartner/Booking.java
@@ -0,0 +1,24 @@
+package com.example.travelpartner;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Booking extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_booking);
+
+
+    }
+
+
+}
