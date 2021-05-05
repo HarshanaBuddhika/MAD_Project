03bd01bb7575a056fc200626e46116b2cc2b3afb
HarshiD123 <harshanidissanayake.1997@gmail.com>
2021-05-06 00:50:26 +0530

    new commit

---
+++ app/src/main/java/com/example/travelpartner/Post_Rent.java
@@ -0,0 +1,14 @@
+package com.example.travelpartner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Post_Rent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post__rent);
+    }
+}
